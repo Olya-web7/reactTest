ba5e17eab30dc24352be9a0b5ed39bfec80befb4
Olya <ola.shy@yahoo.com>
2022-04-10 14:59:41 +0200

    fix

--- src/Directory/index.js
+++ src/Directory/index.js
@@ -1,33 +1,9 @@
 import React from "react";
-import Product1 from './../assets/Product1.png';
-import Product2 from './../assets/Product2.png';
 import './styles.scss';
 
 const Directory = props => {
   return (
-    <div className="directory">
-      <div className="wrap">
-      <div
-        className='item'
-        style={{
-          backgroundImage: `url(${Product1})`
-        }}
-        >
-          <a>
-            WOMEN
-          </a>
-        </div>
-        <div
-          className='item'
-          style={{
-            backgroundImage: `url(${Product2})`
-          }}
-        >
-          <a>
-            MEN
-          </a>
-        </div>
-      </div>
+    <div className="directory">      
     </div>
   );
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,17 @@ import './styles.scss';
 const Header = props => {
   return (
     <header className="header">
+      <div className="wrap">
+      <div className='item'>
+          <a href="www.google.com">WOMEN</a>
+        </div>
+        <div className='item'>
+          <a href="www.google.com">MEN</a>
+        </div>
+        <div className='item'>
+          <a href="www.google.com">KIDS</a>
+        </div>
+      </div>
     </header>
   )
 }
